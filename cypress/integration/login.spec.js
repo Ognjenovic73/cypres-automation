1cca271569288bf566ff587b2817d1dd266b15ff
Ognjenovic73 <ognjenovic.dragan73@gmail.com>
2022-04-29 14:23:42 +0200

    login dopuna

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -11,7 +11,45 @@ describe('login test', () => {
         cy.get('a[href="/login"]').click();
     })
 
-    it.only('login with valid credentials', () => {
+    it('login without email', () => {
+      cy.visit('/login')
+      cy.url().should('include', '/login');
+      cy.get('#password').type('Sith_Lord99');
+      cy.get('button[type="submit"]').click();
+    })
+
+    it('login with email without "@"', () => {
+      cy.visit('/login')
+      cy.url().should('include', '/login');
+      cy.get('#email').type('ognjenovic.dragan73gmail.com');
+      cy.get('#password').type('Sith_Lord99');
+      cy.get('button[type="submit"]').click();
+    })
+
+    it('login with email without ".com"', () => {
+      cy.visit('/login')
+      cy.url().should('include', '/login');
+      cy.get('#email').type('ognjenovic.dragan73@gmail');
+      cy.get('#password').type('Sith_Lord99');
+      cy.get('button[type="submit"]').click();
+    })
+
+    it('login without password', () => {
+      cy.visit('/login')
+      cy.url().should('include', '/login');
+      cy.get('#email').type('ognjenovic.dragan73@gmail.com');
+      cy.get('button[type="submit"]').click();
+    })
+
+    it('login with wrong password', () => {
+      cy.visit('/login')
+      cy.url().should('include', '/login');
+      cy.get('#email').type('ognjenovic.dragan73@gmail.com');
+      cy.get('#password').type('Sith_Lord');
+      cy.get('button[type="submit"]').click();
+    })
+
+    it('login with valid credentials', () => {
       //  cy.get('input[id="email"]');
       cy.visit('/login')
       cy.url().should('include', '/login');
