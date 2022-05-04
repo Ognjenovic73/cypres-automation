c0e18facec5624e127ee8015516c02ced95eb499
Ognjenovic73 <ognjenovic.dragan73@gmail.com>
2022-05-04 15:53:11 +0200

    login negative cases  added

--- cypress/integration/loginPOM.spec.js
+++ cypress/integration/loginPOM.spec.js
@@ -2,11 +2,28 @@ import { loginPage } from "../page_objects/loginPage";
 
 describe('loginPOM', () => {
 
+    beforeEach('visit login page', () => {
+      cy.visit('/login')
+    })
+  /*
+    it('negative case login / bad email', () => {
+    loginPage.login('ognjenovicdragan73@gmail.com','Sith_Lord99')
+    })
+
+    it('negative case login / bad password', () => {
+      loginPage.login('ognjenovic.dragan73@gmail.com','SithLord99')
+    })
+
+    it('negative case login / bad credentials', () => {
+      loginPage.login('ognjenovic.73@gmail.com','Sith99')
+    }) */
+
     it('login with valid data', () => {
-        cy.visit('/login')
+       
       /*  loginPage.emailInput.type('gugagaga@gmail.com')
         loginPage.passwordInput.type('gugagaga1')
         loginPage.submitBtn.click(); */
         loginPage.login('ognjenovic.dragan73@gmail.com','Sith_Lord99');
     })
+    
 })
